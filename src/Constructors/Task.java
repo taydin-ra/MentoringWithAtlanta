dfcdece0fefc9aa078d51b9ae5c09f8abc7c9a06
tugba <ttorun16@gmail.com>
2020-05-12 22:37:24 -0400

    Tomorrow's task is added

---
+++ src/Constructors/Task.java
@@ -0,0 +1,59 @@
+package Constructors;
+
+public class Task {
+    //Java Program to demonstrate the working of banking system
+    //where we deposit and withdraw amount from our account
+    //Creating an account class which has deposit() and withdraw() methods
+
+
+    /*
+    Create the instance variables
+    int acc_no;
+    String name;
+    float amount;
+     */
+
+    /*/
+    Create a constructor with 3 parameters (int a, String name, float amt)
+
+   your instance variables are equal to your parameters of the constructor
+     */
+
+
+    /*
+    Create a method is called deposit
+    one parameter float amnt
+    return type is void
+
+    add your parameter amnt to your instance variable amount
+
+    then print out amnt + "deposited")
+     */
+
+    /*
+    Create a method is called withdraw
+    with parameter float amnt
+    return type is void
+    if  instance amount is less than your  withdraw amnt
+    print out "insufficient balance"
+    if  instance amount is greater than your  withdraw amnt
+    subtract amnt from the instance amoun
+
+    then print out amt + "withdrawn"
+
+     */
+
+    /*
+    create a method checkBalance
+    return type is void
+    print out the "Balance is : " + amount
+     */
+
+    /*
+    and write the toString method to print out the instance variable
+     */
+
+    /*
+    and call these methods in the main method
+     */
+}
